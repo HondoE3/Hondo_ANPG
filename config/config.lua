a27b8fa2ff318e7ca4cd9bb4a8b2083797201a72
Hondo <79276697+HondoE3@users.noreply.github.com>
2025-03-26 00:19:15 +0100

    V1 - ANPG

---
+++ config/config.lua
@@ -0,0 +1,24 @@
+Config = {}
+
+Config.AllowedVehicles = {
+    [`gdargentop`] = true,
+    [`gdrhinehartp`] = true,
+    [`gdbuffalo4p`] = true,
+    [`gdcinquemilap`] = true,
+    [`gddominator3p`] = true,
+    [`gdgranger2p`] = true,
+    [`gdjugularp`] = true,
+    [`gdkomodap`] = true,
+    [`gdnovakp`] = true,
+    [`gdraidenp`] = true,
+    [`gdreblap`] = true,
+    [`gdtailgater2p`] = true,
+    [`gdtorosp`] = true,
+    [`gdvstrp`] = true,
+    -- Tilføj flere køretøjer her
+}
+
+Config.AllowedJobs = {
+    ['police'] = true,
+    -- Tilføj flere jobs her
+}
